7e5dce0dfdbe9fdabfceec3d2728e638bc9c6e64
Alex <67423307+aleeeexcool@users.noreply.github.com>
2025-12-27 16:01:39 +0200

    feat: remove namings

--- contracts/testERC20.sol
+++ contracts/testERC20.sol
@@ -4,7 +4,7 @@ pragma solidity =0.8.23;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract testERC20 is ERC20 {
-    constructor(uint256 initialSupply) ERC20("TEA", "TEA") {
+    constructor(uint256 initialSupply) ERC20("TST", "TST") {
         _mint(msg.sender, initialSupply);
     }
 }
